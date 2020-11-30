503a145360aea4d107d69c272b71757ff2c544cb
Roman Krček <rom4n.krcek@gmail.com>
2020-11-30 19:00:50 +0100

    :art: Added code coments

--- controllers/ketchup_house_controller/ketchup_house_controller.m
+++ controllers/ketchup_house_controller/ketchup_house_controller.m
@@ -2,13 +2,9 @@
 % File:          ketchup_house_controller.m
 % Date:
 % Description:
-% Author:
+% Author: Roman Krček, Karel Hejl
 % Modifications:
 
-% uncomment the next two lines if you want to use
-% MATLAB's desktop to interact with the controller:
-% desktop;
-% keyboard;
 
 TIME_STEP = 64;
 
@@ -24,10 +20,18 @@ wb_motor_set_velocity(motor_right, 1);
 dst_front = wb_robot_get_device('dst_front');
 wb_distance_sensor_enable(dst_front, TIME_STEP)
 
-% main loop:
-% perform simulation steps of TIME_STEP milliseconds
-% and leave the loop when Webots signals the termination
-%
+
+% Example position matrix
+% 1 - represents cans
+% 2 - represents robot
+% 0 0 0 2 0 0 0
+% 0 0 0 0 0 0 0
+% 0 1 0 1 0 1 0
+% 0 0 0 0 0 0 0
+% 0 0 0 0 0 0 0
+% 0 0 0 0 0 0 0
+% 0 0 0 0 0 0 0
+
 while wb_robot_step(TIME_STEP) ~= -1
 
   % read the sensors, e.g.:
