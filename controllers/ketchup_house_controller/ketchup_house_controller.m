fbf0c6c633a38e485080c29e27f52b846435ea8c
Roman Krček <rom4n.krcek@gmail.com>
2020-12-15 23:57:14 +0100

    :sparkles: Add supervisor example
    Added an exmple of supervisor programming that moves one can to different position.

--- controllers/ketchup_house_controller/ketchup_house_controller.m
+++ controllers/ketchup_house_controller/ketchup_house_controller.m
@@ -31,22 +31,32 @@ can_bot = CanBot('motor_left', 'motor_right', 'dst_front', ...
                  robot_position, storage_positions, scan_angle, ...
                  32);
 
-%logging("INFO", 'I am travelling %d lines forward', 64);
+% Example for moving nodes
+while wb_robot_step(32) ~= -1
+	node_can1 = wb_supervisor_node_get_from_def('can1');
+	can1_trans_field = wb_supervisor_node_get_field(node, 'translation');
+	wb_supervisor_field_set_sf_vec3f(can1_trans_field, [-0.45 0.038 -0.24]);
+	wb_supervisor_node_reset_physics(node);
+	if wb_robot_step(32) ~= -1
+		break;
+	end
+end
 
+% Man can collection program
 while true
 
-  cans = can_bot.scan_cans()
+	cans = can_bot.scan_cans()
 
-  if isempty(cans)
-    wb_console_print(sprintf('No more cans to pick up'), WB_STDOUT);
-    can_bot.align([-1 0]);
-    break;
-  end
+	if isempty(cans)
+		wb_console_print(sprintf('No more cans to pick up'), WB_STDOUT);
+		can_bot.align([-1 0]);
+		break;
+	end
 
-  for i = 1:size(cans, 1)
-    target_coords = cans(i, :);
-    can_bot.go_coordinates(target_coords);
-  end
+	for i = 1:size(cans, 1)
+		target_coords = cans(i, :);
+		can_bot.go_coordinates(target_coords);
+	end
 
-  can_bot.store_cans();
+	can_bot.store_cans();
 end
