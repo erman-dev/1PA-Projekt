07d772f90f3ac99986c71a120c09e2ea1e15256e
Karel556 <228694@vutbr.cz>
2024-03-26 17:05:04 +0100

    :rotating_light: Test to run travel
    Testing to run travel algorithm

--- controllers/controller_python/controller_python.py
+++ controllers/controller_python/controller_python.py
@@ -2,35 +2,57 @@
 
 # You may need to import some classes of the controller module. Ex:
 #  from controller import Robot, Motor, DistanceSensor
+
 from controller import Robot
-from can_bot import CanBot
+from can_bot import Canbot
+import numpy as np
+
+
+
+
+print("Hello World!")
+
+
+
 
 storage_positions = [[7, 1], [7, 2], [7, 7], [7, 6]]
-robot_position = [[7, 4]]
+position = [[7, 4]]
 
 # Borders of scan angle
 scan_angle = [[65, 300]]
 
+
 # create the Robot instance.
 robot = Robot()
-can_bot = CanBot(robot, sdfas)
+
+print("Hello World!")
+
 
 # get the time step of the current world.
 timestep = int(robot.getBasicTimeStep())
 
-while robot.step(timestep) != -1:
+
+print("Hello World!")
+
+
+can_bot = Canbot(robot,'motor_left', 'motor_right', 'dst_front_can', 'dst_front_bot', 'compass', 'infra_left', 'infra_right', 64 ,  3, 0.5 , 2, storage_positions, position, 'default_alignment',scan_angle)
+
+
+
+while can_bot.time_step != -1:
+	print("Hello World!")
 	# Initiate scan
-	cans = can_bot.scan_cans()
+	can_bot.travel(1)
 
 	# Exit if there are no more cans
-	if len(cans) == 0:
-		#wb_console_print(sprintf('No more cans to pick up'), WB_STDOUT);
-		can_bot.align(can_bot.default_alignment)
-		break
+	# if len(cans) == 0:
+	# 	#wb_console_print(sprintf('No more cans to pick up'), WB_STDOUT);
+	# 	can_bot.align(can_bot.default_alignment)
+	# 	break
 	
-	# Pickup cans 
-	for can in cans:
-		can_bot.go_coordinates(can)
+	# # Pickup cans 
+	# for can in cans:
+	# 	can_bot.go_coordinates(can)
 	
-	# Store cans
-	can_bot.store_cans()
+	# # Store cans
+	# can_bot.store_cans()
